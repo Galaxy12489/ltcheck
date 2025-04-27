8d805f3d0029d056282881005e90da2429e10f35
Galaxy12489 <kulbezekovstas@gmail.com>
2025-04-28 02:29:54 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "<h1>Hello!</h1>"
-
-if __name__ == "__main__":
-    from waitress import serve 
-    serve(app, host="0.0.0.0", port=8080)
 # Список моделей ноутбуков
 laptops = [
     {"name": "ASUS VivoBook 15", "price": "45,000 - 60,000"},
@@ -24,6 +17,5 @@ def index():
     if request.method == 'POST':
         selected_laptop = request.form.get('laptop')
     return render_template('index.html', laptops=laptops, selected_laptop=selected_laptop)
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080)
