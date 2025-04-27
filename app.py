8e8552721710cf1ee29ef9a879a738b0f682db05
Galaxy12489 <kulbezekovstas@gmail.com>
2025-04-28 01:59:56 +0700

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+# Список моделей ноутбуков
+laptops = [
+    {"name": "ASUS VivoBook 15", "price": "45,000 - 60,000"},
+    {"name": "Lenovo IdeaPad 3", "price": "50,000 - 70,000"},
+    {"name": "HP Pavilion 15", "price": "55,000 - 75,000"},
+    {"name": "Acer Aspire 5", "price": "40,000 - 65,000"},
+    {"name": "Dell Inspiron 15", "price": "60,000 - 80,000"},
+]
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    selected_laptop = None
+    if request.method == 'POST':
+        selected_laptop = request.form.get('laptop')
+    return render_template('index.html', laptops=laptops, selected_laptop=selected_laptop)
+
+if __name__ == '__main__':
+    app.run(debug=True)
